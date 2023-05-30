1abd4a5545e5bed0ec2a34dcee915ae631522382
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2023-05-30 00:23:52 -0500

    script

---
+++ chat_gpt.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = "yourkey"
+
+while True:
+
+    engine_model_gpt3 = "text-davinci-003"
+    prompt = input("Enter new prompt! ")
+
+    if prompt in ['exit', 'salir', 'quit', 'terminar']:
+        break
+
+    completion = openai.Completion.create(
+        engine = engine_model_gpt3,
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature = 0.3
+    )
+
+    response = completion.choices[0].text
+
+    print(response)
